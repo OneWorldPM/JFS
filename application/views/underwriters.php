2a02f04d8fbd3f0c7a0272594b843f952891ad34
Rexter Dayuta <rexterdayuta@gmail.com>
2021-04-14 17:37:31 +0800

    ui update

--- application/views/underwriters.php
+++ application/views/underwriters.php
@@ -19,19 +19,23 @@
         font-weight: 900 !important;
         text-transform: uppercase;
     }
-    .text{
+    .line-space-1{
         line-height: 1;
+    }
+    .text{
+
         margin-top: 10px;
         margin-bottom: 10px;
         margin-right: 10px;
         margin-left: 10px;
     }
     .logos{
-        margin-top: 10px;
-        margin-bottom: 10px;
-        margin-right: 10px;
-        margin-left: 30px;
-        max-width:150px;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        margin-right: 0px;
+        padding: 2px;
+        margin-left:25px;
+        max-width:130px;
         max-height:120px;
         display: inline;
     }
@@ -43,91 +47,93 @@
                 THANK YOU TO OUR GENEROUS UNDERWRITERS
             </div><br>
             <div class="row">
-                <div class="col-md-3 ">
-                    <div style="color: #FFFFFF;background-color: rgba(1,0,7, 0.5);width: 350px;height: 500px">
-                        <div class="text" >
-                        <b> <h4>COUTURE</h4></b>
-                    Charitable Adult Rides & Services, Inc.<br>
-                    Irwin & Joan Jacobs<br>
-                    The Joseph-Ma Family<br>
-                    The Meiselman Family<br>
-                    Marie G. Raftery & Dr. Robert A. Rubenstein<br>
-                    Karen & Jeffrey Silberman<br>
-                    Caryl Lees Witte<br><br>
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-3 col-md-offset-1 ">
+                            <div style="color: #FFFFFF;background-color: rgba(1,0,7, 0.5);width: 350px;height: 500px">
+                                <div class="text line-space-1" >
+                                    <b> <h4>COUTURE</h4></b>
+                                    Charitable Adult Rides & Services, Inc.<br>
+                                    Irwin & Joan Jacobs<br>
+                                    The Joseph-Ma Family<br>
+                                    The Meiselman Family<br>
+                                    Marie G. Raftery & Dr. Robert A. Rubenstein<br>
+                                    Karen & Jeffrey Silberman<br>
+                                    Caryl Lees Witte<br><br>
 
-                        <b><h4>DESIGNER</h4></b>
-                    Elaine & Rick Barton<br>
-                    Marcia Foster Hazan & Mark Cammell<br>
-                    Marjorie & Sheldon Derezin<br>
-                    Roxy & Mick Fredericks<br>
-                    Kathy & Mark Jones<br>
-                    Lisa & Gary Levine| Lindsay & Mitch Surowitz | Arthur J. Gallagher & Co.<br>
-                    Sylvia Liwerant<br><br>
+                                    <b><h4>DESIGNER</h4></b>
+                                    Elaine & Rick Barton<br>
+                                    Marcia Foster Hazan & Mark Cammell<br>
+                                    Marjorie & Sheldon Derezin<br>
+                                    Roxy & Mick Fredericks<br>
+                                    Kathy & Mark Jones<br>
+                                    Lisa & Gary Levine| Lindsay & Mitch Surowitz | Arthur J. Gallagher & Co.<br>
+                                    Sylvia Liwerant<br><br>
 
-                        <b><h4>BESPOKE</h4></b>
-                    Deborah Bucksbaum<br>
-                    Capital City Auto Auction, Inc.<br>
-                    City National Bank<br>
-                    Susan Shmalo & Gene H. Carswell<br>
-                    Liz Nederlander Coden & Daniel Coden, M.D.<br>
-                    Linda & Edward Janon<br>
-                    Susan & David Kabakoff<br>
-                    LourdMurray Delphi<br>
-                    Alan Haubenstock, Brian Haubenstock & Lori Shearer
-                    Karin & Tony Toranto<br>
-                    Emma & Leo Zuckerman
+                                    <b><h4>BESPOKE</h4></b>
+                                    Deborah Bucksbaum<br>
+                                    Capital City Auto Auction, Inc.<br>
+                                    City National Bank<br>
+                                    Susan Shmalo & Gene H. Carswell<br>
+                                    Liz Nederlander Coden & Daniel Coden, M.D.<br>
+                                    Linda & Edward Janon<br>
+                                    Susan & David Kabakoff<br>
+                                    LourdMurray Delphi<br>
+                                    Alan Haubenstock, Brian Haubenstock & Lori Shearer
+                                    Karin & Tony Toranto<br>
+                                    Emma & Leo Zuckerman
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-md-offset-1">
+                            <div style="color: #FFFFFF;background-color: white;width: 380px;height: 500px">
+                                    <a href="https://careasy.org/home"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CARS_Logo_Full_Horz_CMYK-01.jpg" class="logos"></a>
+                                    <a href="https://www.century21.com/real-estate-agent/profile/caryl-lees-witte-P10304096"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CLW.jpg" class="logos"></a>
+                                    <a href="https://capitalcityautoauction.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Capital City Auto Auction logo - color.pdf.jpg" class="logos"></a>
+                                    <a href="https://www.cnb.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CNB-RBC Integrated Logo_RGB.jpg" class="logos"></a>
+                                    <a href="https://www.pacificsothebysrealty.com/agent/621-a-2492-4030139/liz-coden"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Liz Coden.jpg" class="logos"></a>
+                                    <a href="https://www.lourdmurray.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/LM Delphi Stacked Grey Navy.jpg" class="logos"></a>
+                                    <a href="https://www.century21.com/real-estate-agent/profile/nancy-beck-P25127112"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/NancyBeck_bannerhighresolution.jpg" class="logos"></a>
+                                    <a href=""><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/MollysAngel.jpg" class="logos"></a>
+                                    <a href="https://centurawealth.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CenturaWealthAdvisoryLogo_ontransparentbackground.png" class="logos"></a>
+                                    <a href="https://www.pathfinderfunds.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/pathfinder-logo-tagline (1).jpg" class="logos"></a>
+                                    <a href="https://www.seacrestvillage.org/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/SFlogo2016.jpg" class="logos"></a>
+                                    <a href="https://www.seacrestvillage.org/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/SVRClogo_purple_19.jpg" class="logos"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-md-offset-0" style="margin-right: ">
+                            <div style="color: #FFFFFF;background-color: rgba(1,0,7, 0.5);width: 350px;height: 500px">
+                                <div class="text" style="line-height: 1.2">
+                                    <b><h4>VINTAGE</h4></b><br>
+                                    Nancy Beck | Century 21 Award<br>
+                                    Judy & Larry Belinsky<br>
+                                    Sheila Belinsky | Molly’s Angels<br>
+                                    Rabbi Devorah Marcus & Sebastian Eickholt<br>
+                                    April & Mathew Fink | Comfort Keepers<br>
+                                    Gabrielle Hochberg<br>
+                                    Dr. Stuart Karasik<br>
+                                    Marge Katleman<br>
+                                    Theresa Dupuis & Gary Kornfeld<br>
+                                    Mimi Lee<br>
+                                    Dana Levin | Centura Wealth Advisory<br>
+                                    Dr. Marshall Littman<br>
+                                    Dr. Howard & Barbara Milstein<br>
+                                    Dr. Charles & Ilene Mittman<br>
+                                    Victoria Pappas<br>
+                                    Manheim<br>
+                                    Mitch & Elizabeth Siegler | Pathfinder Partners<br>
+                                    Sheryl & Bill Rowling<br>
+                                    Seacrest Village & Seacrest Foundation<br>
+                                    Thompson Search<br>
+                                    Fern & Lee Siegel<br>
+                                    Jill & Mark Spitzer<br>
+                                    Rachel & Adam Welland<br>
+                                    Joellyn & Ron Zollman<br>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-4 ">
-                    <div style="color: #FFFFFF;background-color: white;width: 580px;height: 500px">
-                        <div >
-                            <a href="https://careasy.org/home"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CARS_Logo_Full_Horz_CMYK-01.jpg" class="logos"></a>
-                            <a href="https://www.century21.com/real-estate-agent/profile/caryl-lees-witte-P10304096"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CLW.jpg" class="logos"></a>
-                            <a href="https://capitalcityautoauction.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Capital City Auto Auction logo - color.pdf.jpg" class="logos"></a>
-                            <a href="https://www.cnb.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CNB-RBC Integrated Logo_RGB.jpg" class="logos"></a>
-                            <a href="https://www.pacificsothebysrealty.com/agent/621-a-2492-4030139/liz-coden"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Liz Coden.jpg" class="logos"></a>
-                            <a href="https://www.lourdmurray.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/LM Delphi Stacked Grey Navy.jpg" class="logos"></a>
-                            <a href="https://www.century21.com/real-estate-agent/profile/nancy-beck-P25127112"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/NancyBeck_bannerhighresolution.jpg" class="logos"></a>
-                            <a href=""><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/MollysAngel.jpg" class="logos"></a>
-                            <a href="https://centurawealth.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CenturaWealthAdvisoryLogo_ontransparentbackground.png" class="logos"></a>
-                            <a href="https://www.pathfinderfunds.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/pathfinder-logo-tagline (1).jpg" class="logos"></a>
-                            <a href="https://www.seacrestvillage.org/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/SFlogo2016.jpg" class="logos"></a>
-                            <a href="https://www.seacrestvillage.org/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/SVRClogo_purple_19.jpg" class="logos"></a>
 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-3 col-md-offset-1" style="margin-right: ">
-                    <div style="color: #FFFFFF;background-color: rgba(1,0,7, 0.5);width: 400px;height: 500px">
-                        <div class="text">
-                    <b><h4>VINTAGE</h4></b><br>
-                    Nancy Beck | Century 21 Award<br>
-                    Judy & Larry Belinsky<br>
-                    Sheila Belinsky | Molly’s Angels<br>
-                    Rabbi Devorah Marcus & Sebastian Eickholt<br>
-                    April & Mathew Fink | Comfort Keepers<br>
-                    Gabrielle Hochberg<br>
-                    Dr. Stuart Karasik<br>
-                    Marge Katleman<br>
-                    Theresa Dupuis & Gary Kornfeld<br>
-                    Mimi Lee<br>
-                    Dana Levin | Centura Wealth Advisory<br>
-                    Dr. Marshall Littman<br>
-                    Dr. Howard & Barbara Milstein<br>
-                    Dr. Charles & Ilene Mittman<br>
-                    Victoria Pappas<br>
-                    Manheim<br>
-                    Mitch & Elizabeth Siegler | Pathfinder Partners<br>
-                    Sheryl & Bill Rowling<br>
-                    Seacrest Village & Seacrest Foundation<br>
-                    Thompson Search<br>
-                    Fern & Lee Siegel<br>
-                    Jill & Mark Spitzer<br>
-                    Rachel & Adam Welland<br>
-                    Joellyn & Ron Zollman<br>
-                    </div>
-                    </div>
                 </div>
             </div>
         </div>
