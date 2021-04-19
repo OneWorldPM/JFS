7c3f9242ed7eaceb2930a56d00e037ea0a742858
Rexter Dayuta <rexterdayuta@gmail.com>
2021-04-19 23:37:30 +0800

    underwriters update

--- application/views/underwriters.php
+++ application/views/underwriters.php
@@ -1,8 +1,8 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
+
 <style>
-    body{
-        margin-top: -50px;
-        background-size: cover;
-    }
+
     h4{
         color:white;
     }
@@ -20,7 +20,10 @@
         text-transform: uppercase;
     }
     .line-space-1{
-        line-height: 1;
+        line-height: 1.2;
+    }
+    .line-space-1-2{
+        line-height: 1.5;
     }
     .text{
 
@@ -34,14 +37,109 @@
         margin-bottom: 0px;
         margin-right: 0px;
         padding: 2px;
-        margin-left:25px;
-        max-width:130px;
-        max-height:120px;
+        margin-left:45px;
+        max-width:110px;
+        max-height:65px;
         display: inline;
     }
     .parallax{
         height:auto!important;
         padding-bottom: 20%;
+    }
+
+    .box1 {
+        color: #FFFFFF;
+        background-color: rgba(1, 0, 7, 0.8);
+        width: 400px;
+        height: 650px;
+        overflow: auto;
+    }
+
+    .box2 {
+        color: #FFFFFF;
+        background-color: white;
+        width: 400px;
+        height: 650px;
+
+        overflow: auto;
+
+    }
+
+    .col{
+
+        margin: auto;
+        flex: 50%;
+        padding: 10px;
+        height: 300px;
+    }
+
+    .row {
+        display: flex;
+        justify-content: center;
+    }
+
+
+    @media (min-width: 768px) and (max-width: 1000px)  {
+        .parallax{
+
+
+        }
+
+    }
+
+    @media (min-width: 1000px) and (max-width: 1400px)  {
+        #home_first_section{
+
+
+        }
+
+    }
+
+    @media (min-width: 1400px) and (max-width: 1600px)  {
+        #home_first_section{
+
+        }
+
+    }
+
+    @media (min-width: 1600px) and (max-width: 1800px)  {
+        #home_first_section{
+
+        }
+    }
+
+    @media (min-width: 1800px) and (max-width: 2200px)  {
+        #home_first_section{
+
+        }
+    }
+
+    @media (min-width: 2200px) and (max-width: 2800px)  {
+        #home_first_section{
+
+        }
+    }
+    @media (min-width: 2800px) and (max-width: 3200px)  {
+        #home_first_section{
+
+        }
+    }
+
+    @media (min-width: 3200px) and (max-width: 4200px)  {
+        #home_first_section{
+
+        }
+    }
+
+    @media (min-width: 4200px) and (max-width: 6000px)  {
+        #home_first_section{
+
+        }
+    }
+    .parallax{
+        padding-bottom: 50%;
+        margin-top: -50px;
+        background-size: cover;
     }
 </style>
 <section class="parallax" style="background-image: url(<?= base_url() ?>front_assets/images/underwriters/Site_Background_04_Underwriters.jpg);">
@@ -53,94 +151,108 @@
             <div class="row">
                 <div class="col-md-10">
                     <div class="row">
-                        <div class="col-md-3 col-md-offset-1 ">
-                            <div style="color: #FFFFFF; background-color: rgba(1,0,7, 0.5);width: 350px;height: 500px">
+                        <div class="col">
+                            <div class="box1">
                                 <div class="text line-space-1" >
-                                    <b> <h4>COUTURE</h4></b>
+                                    <b> <h4>COUTURE $18,000 </h4></b>
                                     Charitable Adult Rides & Services, Inc.<br>
-                                    Irwin & Joan Jacobs<br>
+                                    Irwin &; Joan Jacobs<br>
                                     The Joseph-Ma Family<br>
                                     The Meiselman Family<br>
                                     Marie G. Raftery & Dr. Robert A. Rubenstein<br>
                                     Karen & Jeffrey Silberman<br>
                                     Caryl Lees Witte<br><br>
 
-                                    <b><h4>DESIGNER</h4></b>
+                                    <b><h4>DESIGNER  $10,000</h4></b>
                                     Elaine & Rick Barton<br>
+                                    Gallagher | Lisa & Gary Levine | Lindsay & Mitch Surowitz <br>
                                     Marcia Foster Hazan & Mark Cammell<br>
                                     Marjorie & Sheldon Derezin<br>
                                     Roxy & Mick Fredericks<br>
                                     Kathy & Mark Jones<br>
-                                    Lisa & Gary Levine| Lindsay & Mitch Surowitz | Arthur J. Gallagher & Co.<br>
-                                    Sylvia Liwerant<br><br>
+                                    Sylvia Liwerant<br>
+                                    Sharon & David Wax<br><br>
 
-                                    <b><h4>BESPOKE</h4></b>
+                                    <b><h4>BESPOKE  $5,000 </h4></b>
                                     Deborah Bucksbaum<br>
                                     Capital City Auto Auction, Inc.<br>
                                     City National Bank<br>
                                     Susan Shmalo & Gene H. Carswell<br>
                                     Liz Nederlander Coden & Daniel Coden, M.D.<br>
+                                    Laura Galinson & Jodi Diamond<br>
                                     Linda & Edward Janon<br>
+                                    Emily & Chris Jennewein | Times of San Diego<br>
                                     Susan & David Kabakoff<br>
                                     LourdMurray Delphi<br>
-                                    Alan Haubenstock, Brian Haubenstock & Lori Shearer
+                                    Lauren & Robert Resnik<br>
+                                    Alan Haubenstock, Brian Haubenstock & Lori Shearer<br>
                                     Karin & Tony Toranto<br>
-                                    Emma & Leo Zuckerman
+                                    Emma & Leo Zuckerman<br>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-4 col-md-offset-1">
-                            <div style="color: #FFFFFF;background-color: white;width: 380px;height: 500px; margin-top: 10px">
+                        <div class="col">
+                            <div class="box2">
                                 <div>
                                     <a href="https://careasy.org/home" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CARS_Logo_Full_Horz_CMYK-01.jpg" class="logos"></a>
                                     <a href="https://www.century21.com/real-estate-agent/profile/caryl-lees-witte-P10304096" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CLW.jpg" class="logos"></a>
+                                    <a href="https://www.ajg.com" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Gallagher-logo-400x204.png" class="logos"></a>
                                     <a href="https://capitalcityautoauction.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Capital City Auto Auction logo - color.pdf.jpg" class="logos"></a>
                                     <a href="https://www.cnb.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CNB-RBC Integrated Logo_RGB.jpg" class="logos"></a>
                                     <a href="https://www.pacificsothebysrealty.com/agent/621-a-2492-4030139/liz-coden" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Liz Coden.jpg" class="logos"></a>
+                                    <a href="https://timesofsandiego.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/TImes_of_San_Diego_logo.png" class="logos"></a>
                                     <a href="https://www.lourdmurray.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/LM Delphi Stacked Grey Navy.jpg" class="logos"></a>
                                     <a href="https://www.century21.com/real-estate-agent/profile/nancy-beck-P25127112" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/NancyBeck_bannerhighresolution.jpg" class="logos"></a>
-                                    <a href="" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/MollysAngel.jpg" class="logos"></a>
+                                    <a ><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/MollysAngel.jpg" class="logos"></a>
+                                    <a href="https://www.comfortkeepers.com/"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CK_logo_vert_EHS.png" class="logos"></a>
                                     <a href="https://centurawealth.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/CenturaWealthAdvisoryLogo_ontransparentbackground.png" class="logos"></a>
+                                    <a href="https://www.manheim.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/manheim-logo-400x98.jpg" class="logos"></a>
                                     <a href="https://www.pathfinderfunds.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/pathfinder-logo-tagline (1).jpg" class="logos"></a>
+                                    <a href="https://www.sdfoundation.org/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/sdf.jpg" class="logos"></a>
                                     <a href="https://www.seacrestvillage.org/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/SFlogo2016.jpg" class="logos"></a>
                                     <a href="https://www.seacrestvillage.org/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/SVRClogo_purple_19.jpg" class="logos"></a>
-
+                                    <a href="https://www.thompsonsearch.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Thompson.png" class="logos"></a>
+                                    <a href="https://jcfsandiego.org/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/jewish-community-foundation-400x267.jpg" class="logos"></a>
+                                    <a href="https://sycuantribe.com/" target="_blank"><img src="<?= base_url() ?>front_assets/images/underwriters/Underwritter_Main_Logos/Sycuan-logo-338x400.jpg" class="logos"></a>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-3 col-md-offset-0" style="margin-right: ">
-                            <div style="color: #FFFFFF;background-color: rgba(1,0,7, 0.5);width: 350px;height: 500px">
-                                <div class="text" style="line-height: 1.2">
-                                    <b><h4>VINTAGE</h4></b><br>
-                                    Nancy Beck | Century 21 Award<br>
+                        <div class="col">
+                            <div class="box1">
+                                <div class="text line-space-1-2">
+                                    <b><h4>VINTAGE  $2,500 </h4></b>
+                                    Nancy Beck | Century 21 Award <br>
                                     Judy & Larry Belinsky<br>
-                                    Sheila Belinsky | Molly’s Angels<br>
-                                    Rabbi Devorah Marcus & Sebastian Eickholt<br>
-                                    April & Mathew Fink | Comfort Keepers<br>
-                                    Gabrielle Hochberg<br>
-                                    Dr. Stuart Karasik<br>
+                                    Sheila Belinsky | Molly’s Angels <br>
+                                    Rabbi Devorah Marcus & Sebastian Eickholt <br>
+                                    April & Mathew Fink | Comfort Keepers Homecare <br>
+                                    Gabrielle Hochberg <br>
+                                    Dr. Stuart Karasik <br>
                                     Marge Katleman<br>
                                     Theresa Dupuis & Gary Kornfeld<br>
                                     Mimi Lee<br>
+
                                     Dana Levin | Centura Wealth Advisory<br>
                                     Dr. Marshall Littman<br>
+                                    Manheim<br>
                                     Dr. Howard & Barbara Milstein<br>
                                     Dr. Charles & Ilene Mittman<br>
                                     Victoria Pappas<br>
-                                    Manheim<br>
                                     Mitch & Elizabeth Siegler | Pathfinder Partners<br>
                                     Sheryl & Bill Rowling<br>
+                                    The San Diego Foundation<br>
                                     Seacrest Village & Seacrest Foundation<br>
                                     Thompson Search<br>
                                     Fern & Lee Siegel<br>
+                                    Beth Sirull & Jonathan Shuster<br>
                                     Jill & Mark Spitzer<br>
+                                    Sycuan Band of the Kumeyaay Nation<br>
                                     Rachel & Adam Welland<br>
                                     Joellyn & Ron Zollman<br>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
